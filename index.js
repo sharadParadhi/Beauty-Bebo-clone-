76d9e98e007238f3ab117a8832dd6b3d0abdca7a
sharad paradhi <sharadparadhi2124@gmail.com>
2022-12-16 00:00:09 +0530

    day3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+
+
+
+  let container=document.getElementById("product-container");
+
+  fetch("https://6398974efe03352a94d6e5fd.mockapi.io/product/beauty")
+  .then((resObj)=>{
+    return resObj.json()
+  })
+  .then((actData)=>{
+    console.log(actData)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+
+  function display(data){
+    data.forEach((element,index)=>{
+
+      let productcontainer=document.createElement("div")
+      let img=document.createElement("img")
+      
+    })
+  }
